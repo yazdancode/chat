77408ba8b5e3ecc12a9dc998d1ef3ce8230bb860
yazdancode <yshabanei@gmail.com>
2024-12-13 06:19:55 -0800

    new code

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("Post.urls"))]
+
+if settings.DEBUG:  # فقط در حالت توسعه
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
