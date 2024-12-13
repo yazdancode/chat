9e4b1f6a9a42fcbae49fc6e5c9d58ada22425027
yazdancode <yshabanei@gmail.com>
2024-12-13 10:50:21 -0800

    code error dareh

--- Chat/urls.py
+++ Chat/urls.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("Post.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("Post.urls")),
+    path("accounts/", include("allauth.urls")),
+]
 
 if settings.DEBUG:  # فقط در حالت توسعه
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
